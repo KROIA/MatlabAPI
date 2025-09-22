c9909fac1ade6c3d03312b127093d81ad33fc06c
Alex <alexkrieg@gmx.ch>
2025-09-22 20:55:42 +0200

    -

--- MatlabAPI.cmake
+++ MatlabAPI.cmake
@@ -20,10 +20,24 @@ function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
     message("Downloading dependency: ${LIB_NAME} from: ${GIT_REPO} tag: ${GIT_TAG}")
     FetchContent_MakeAvailable(${LIB_NAME})
 
+    find_package(Matlab REQUIRED COMPONENTS ENG_LIBRARY MX_LIBRARY MAT_LIBRARY)
+    
+    # Include directories
+    include_directories(${Matlab_INCLUDE_DIRS})
+    
+    set(MATLAB_LIBS
+        ${Matlab_ENGINE_LIBRARY}
+        ${Matlab_DATAARRAY_LIBRARY}
+        ${Matlab_ENG_LIBRARY}
+        ${Matlab_MX_LIBRARY}
+        ${Matlab_MEX_LIBRARY}
+        ${Matlab_MAT_LIBRARY}
+    )
+
     # Add this library to the specific profiles of this project
-    list(APPEND DEPS_FOR_SHARED_LIB ${LIB_NAME}_shared)
-    list(APPEND DEPS_FOR_STATIC_LIB ${LIB_NAME}_static)
-    list(APPEND DEPS_FOR_STATIC_PROFILE_LIB ${LIB_NAME}_static_profile) # only use for static profiling profile
+    list(APPEND DEPS_FOR_SHARED_LIB ${LIB_NAME}_shared ${MATLAB_LIBS})
+    list(APPEND DEPS_FOR_STATIC_LIB ${LIB_NAME}_static ${MATLAB_LIBS})
+    list(APPEND DEPS_FOR_STATIC_PROFILE_LIB ${LIB_NAME}_static_profile ${MATLAB_LIBS}) # only use for static profiling profile
 
     set(${LIBRARY_MACRO_NAME} "${${LIBRARY_MACRO_NAME}};${LIB_MACRO_NAME}" PARENT_SCOPE)
     set(${SHARED_LIB} "${${SHARED_LIB}};${DEPS_FOR_SHARED_LIB}" PARENT_SCOPE)
